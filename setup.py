a3fd8fb59c66712c7d1de0764491fe4fd444bf9d
kadirnar <kadir.nar@hotmail.com>
2023-11-30 00:49:25 +0300

    🔧 Update pre-commit hooks and package information

--- setup.py
+++ setup.py
@@ -20,30 +20,23 @@ def get_long_description():
 
 def get_version():
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    version_file = os.path.join(current_dir, 'whisperplus', '__init__.py')
+    version_file = os.path.join(current_dir, 'animeup', '__init__.py')
     with open(version_file, encoding='utf-8') as f:
         return re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', f.read(), re.M).group(1)
 
 
 def get_author():
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    init_file = os.path.join(current_dir, 'whisperplus', '__init__.py')
+    init_file = os.path.join(current_dir, 'animeup', '__init__.py')
     with open(init_file, encoding='utf-8') as f:
         return re.search(r'^__author__ = [\'"]([^\'"]*)[\'"]', f.read(), re.M).group(1)
 
 
 def get_license():
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    init_file = os.path.join(current_dir, 'whisperplus', '__init__.py')
+    init_file = os.path.join(current_dir, 'animeup', '__init__.py')
     with open(init_file, encoding='utf-8') as f:
         return re.search(r'^__license__ = [\'"]([^\'"]*)[\'"]', f.read(), re.M).group(1)
-
-
-def get_license():
-    current_dir = os.path.abspath(os.path.dirname(__file__))
-    init_file = os.path.join(current_dir, 'whisperplus', '__init__.py')
-    with open(init_file, encoding='utf-8') as f:
-        return re.search(r'^__package__ = [\'"]([^\'"]*)[\'"]', f.read(), re.M).group(1)
 
 
 setuptools.setup(
